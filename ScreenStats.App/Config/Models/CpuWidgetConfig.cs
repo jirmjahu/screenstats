631fb0664bdc8b7246548e7346ef5a2992040e32
jirmjahu <159639233+jirmjahu@users.noreply.github.com>
2026-04-19 13:43:13 +0200

    Remove UpdateInterval option

--- ScreenStats.App/Config/Models/CpuWidgetConfig.cs
+++ ScreenStats.App/Config/Models/CpuWidgetConfig.cs
@@ -3,5 +3,4 @@
 public class CpuWidgetConfig : WidgetConfig
 {
     public string Label { get; set; }
-    public int UpdateInterval { get; set; } = 1000;
 }
--- ScreenStats.App/Config/Models/RamWidgetConfig.cs
+++ ScreenStats.App/Config/Models/RamWidgetConfig.cs
@@ -3,5 +3,4 @@
 public class RamWidgetConfig : WidgetConfig
 {
     public string Label { get; set; }
-    public int UpdateInterval { get; set; } = 1000;
 }
