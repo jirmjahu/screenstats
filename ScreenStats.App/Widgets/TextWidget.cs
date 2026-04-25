99b7d880651b705c079ef0d521853dbb981b5dcd
jirmjahu <159639233+jirmjahu@users.noreply.github.com>
2026-04-25 14:15:20 +0200

    Make TextWidget not an UpdateableWidget

--- ScreenStats.App/Widgets/TextWidget.cs
+++ ScreenStats.App/Widgets/TextWidget.cs
@@ -1,37 +1,13 @@
-﻿using System.ComponentModel;
-using System.Windows.Controls;
+﻿using System.Windows.Controls;
 using ScreenStats.App.Controls;
 
 namespace ScreenStats.App.Widgets;
 
-public class TextWidget(string content, string fontFamily, double fontSize) : UpdateableWidget, INotifyPropertyChanged
+public class TextWidget(string content, string fontFamily, double fontSize) : Widget
 {
-    private string _content = content;
-
-    public string Content
-    {
-        get => _content;
-        set
-        {
-            if (_content == value)
-            {
-                return;
-            }
-
-            _content = value;
-            PropertyChanged?.Invoke(this, new(nameof(Content)));
-        }
-    }
-
-    public event PropertyChangedEventHandler? PropertyChanged;
-
+    public string Content { get; set; } = content;
     public string FontFamily { get; set; } = fontFamily;
     public double FontSize { get; set; } = fontSize;
-
-    public override void Update()
-    {
-        Content += "1";
-    }
 
     public override UserControl GetControl()
     {
