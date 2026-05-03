aa12ce063dbcb8f4d22589412c0d8e4be33383e5
jirmjahu <159639233+jirmjahu@users.noreply.github.com>
2026-05-03 16:51:00 +0200

    Adjust AppPaths to prioritize installer over portable mode

--- ScreenStats.App/AppPaths.cs
+++ ScreenStats.App/AppPaths.cs
@@ -4,9 +4,9 @@ namespace ScreenStats.App;
 
 public static class AppPaths
 {
-    private static readonly string BasePath = BuildInfo.GetMode() == BuildInfo.AppMode.Portable
-        ? AppDomain.CurrentDomain.BaseDirectory
-        : Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData), "ScreenStats");
+    private static readonly string BasePath = BuildInfo.GetMode() == BuildInfo.AppMode.Installer
+        ? Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData), "ScreenStats")
+        : AppDomain.CurrentDomain.BaseDirectory;
 
     public static string ConfigDirectory => Path.Combine(BasePath, "config");
     public static string ConfigFile => Path.Combine(ConfigDirectory, "config.ini");
